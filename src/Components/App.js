221a035944879b27c8b4cd31c8c8ae171d708465
bcherrera14 <bcherrera14@gmail.com>
2021-02-13 12:15:47 -0800

    create header component

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,10 +3,13 @@ import { BrowserRouter, Route, Link } from 'react-router-dom';
 import TweetLanding from './tweets/TweetLanding';
 import TweetFavorites from './tweets/TweetFavorites';
 import TweetSearch from './tweets/TweetSearch';
+import Header from './Header';
 
 const App = () => {
 	return (
 		<BrowserRouter>
+			<Header />
+
 			<div>
 				<Route path="/" exact component={TweetLanding} />
 				<Route path="/tweet/search" exact component={TweetSearch} />
