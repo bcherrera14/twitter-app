37d7e2842aefc5c55d98bcd37c7676391b529eac
bcherrera14 <bcherrera14@gmail.com>
2021-02-15 09:14:58 -0800

    Navbar Links

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import TweetLanding from './tweets/TweetLanding';
 import TweetFavorites from './tweets/TweetFavorites';
 import TweetSearch from './tweets/TweetSearch';
@@ -7,15 +7,16 @@ import Header from './Header';
 
 const App = () => {
 	return (
-		<BrowserRouter>
-			<Header />
-
-			<div>
-				<Route path="/" exact component={TweetLanding} />
-				<Route path="/tweet/search" exact component={TweetSearch} />
-				<Route path="/tweet/favorites" exact component={TweetFavorites} />
-			</div>
-		</BrowserRouter>
+		<div className="container">
+			<BrowserRouter>
+				<div>
+					<Header />
+					<Route path="/" exact component={TweetLanding} />
+					<Route path="/tweet/search" exact component={TweetSearch} />
+					<Route path="/tweet/favorites" exact component={TweetFavorites} />
+				</div>
+			</BrowserRouter>
+		</div>
 	);
 };
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { Navbar, Nav, Form, Button, FormControl, NavDropdown } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-	return <div>Header</div>;
+	return (
+		<Navbar bg="light" expand="lg">
+			<Link className="navbar-brand" to="/">
+				Daily Tweets
+			</Link>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="ml-auto">
+					<Link className="nav-link" to="/tweet/search">
+						Search
+					</Link>
+					<Link className="nav-link" to="/tweet/favorites">
+						Favorties
+					</Link>
+				</Nav>
+			</Navbar.Collapse>
+		</Navbar>
+	);
 };
 
 export default Header;
