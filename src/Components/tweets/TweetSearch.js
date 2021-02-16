2eda276dfeb28bbd7779c0555f697eebe9ac0b74
bcherrera14 <bcherrera14@gmail.com>
2021-02-15 17:50:58 -0800

    search by username or keyword

--- src/Components/tweets/TweetSearch.js
+++ src/Components/tweets/TweetSearch.js
@@ -19,11 +19,21 @@ class TweetSearch extends React.Component {
 	onFormSubmit(e) {
 		e.preventDefault();
 		const searchTerm = e.target.searchTerm.value;
-		const response = tweetData.filter((tweet) => tweet.firstName.toLowerCase() === searchTerm.toLowerCase());
+		const searchCategory = e.target.inlineFormCustomSelect.value;
+		let response = [];
+		if (searchCategory === 'username') {
+			response = tweetData.filter((tweet) => tweet.firstName.toLowerCase() === searchTerm.toLowerCase());
+		} else if (searchCategory === 'keyword') {
+			tweetData.forEach((tweet) => {
+				if (tweet.textContent.toLowerCase().includes(searchTerm.toLowerCase())) {
+					response.push(tweet);
+				}
+			});
+		}
 		this.setState({
 			tweetData: response
 		});
-		console.log(searchTerm);
+		console.log(e.target.inlineFormCustomSelect.value);
 		// console.log(response);
 	}
 
@@ -33,6 +43,15 @@ class TweetSearch extends React.Component {
 			<div className="d-flex justify-content-center flex-column align-items-center">
 				<Form onSubmit={this.onFormSubmit}>
 					<Form.Row className="align-items-center">
+						<Col xs="auto" className="my-1">
+							<Form.Label className="mr-sm-2" htmlFor="inlineFormCustomSelect" srOnly>
+								Preference
+							</Form.Label>
+							<Form.Control as="select" className="mr-sm-2" id="inlineFormCustomSelect" custom>
+								<option value="username">Username</option>
+								<option value="keyword">Keyword</option>
+							</Form.Control>
+						</Col>
 						<Col className="my-1">
 							<Form.Label htmlFor="inlineFormInputName" srOnly>
 								Search
@@ -41,7 +60,8 @@ class TweetSearch extends React.Component {
 								<Form.Control
 									id="search-input"
 									name="searchTerm"
-									placeholder="Search for username or keyword"
+									autoComplete="off"
+									placeholder="Search for tweets"
 								/>
 								<span>
 									<i className="fas fa-search" />
