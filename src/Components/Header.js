8097a08edfd3a2d570b482a3b68812c5669ae545
bcherrera14 <bcherrera14@gmail.com>
2021-02-16 14:02:23 -0800

    add styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,21 +4,23 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
 	return (
-		<Navbar bg="light" expand="lg">
-			<Link className="navbar-brand" to="/">
-				Daily Tweets
-			</Link>
-			<Navbar.Toggle aria-controls="basic-navbar-nav" />
-			<Navbar.Collapse id="basic-navbar-nav">
-				<Nav className="ml-auto">
-					<Link className="nav-link" to="/tweet/favorites">
-						Trending
-					</Link>
-					<Link className="nav-link" to="/tweet/search">
-						Search
-					</Link>
-				</Nav>
-			</Navbar.Collapse>
+		<Navbar bg="dark" expand="lg" className="fixed-top navbar-dark">
+			<div className="container">
+				<Link className="navbar-brand" to="/">
+					Daily Tweets
+				</Link>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ml-auto">
+						<Link className="nav-link" to="/tweet/favorites">
+							Trending
+						</Link>
+						<Link className="nav-link" to="/tweet/search">
+							Search
+						</Link>
+					</Nav>
+				</Navbar.Collapse>
+			</div>
 		</Navbar>
 	);
 };
--- src/Components/tweets/TweetSearch.js
+++ src/Components/tweets/TweetSearch.js
@@ -39,39 +39,42 @@ class TweetSearch extends React.Component {
 	render() {
 		console.log(this.state.tweetData);
 		return (
-			<div className="d-flex justify-content-center flex-column align-items-center">
-				<Form onSubmit={this.onFormSubmit}>
-					<Form.Row className="align-items-center">
-						<Col xs="auto" className="my-1">
-							<Form.Label className="mr-sm-2" htmlFor="inlineFormCustomSelect" srOnly>
-								Preference
-							</Form.Label>
-							<Form.Control as="select" className="mr-sm-2" id="inlineFormCustomSelect" custom>
-								<option value="username">Username</option>
-								<option value="keyword">Keyword</option>
-							</Form.Control>
-						</Col>
-						<Col className="my-1">
-							<Form.Label htmlFor="inlineFormInputName" srOnly>
-								Search
-							</Form.Label>
-							<div className="search">
-								<Form.Control
-									id="search-input"
-									name="searchTerm"
-									autoComplete="off"
-									placeholder="Search for tweets"
-								/>
-								<span>
-									<i className="fas fa-search" />
-								</span>
-							</div>
-						</Col>
-						<Col xs="auto" className="my-1">
-							<Button type="submit">Search</Button>
-						</Col>
-					</Form.Row>
-				</Form>
+			<div id="search-content" className="d-flex justify-content-center flex-column align-items-center mt-5">
+				<div className="search-card card p-3 d-flex justify-content-center align-items-center">
+					<Form onSubmit={this.onFormSubmit}>
+						<Form.Row className="align-items-center">
+							<Col xs="auto" className="my-1">
+								<Form.Label className="mr-sm-2" htmlFor="inlineFormCustomSelect" srOnly>
+									Preference
+								</Form.Label>
+								<Form.Control as="select" className="mr-sm-2" id="inlineFormCustomSelect" custom>
+									<option value="username">Username</option>
+									<option value="keyword">Keyword</option>
+								</Form.Control>
+							</Col>
+							<Col className="my-1">
+								<Form.Label htmlFor="inlineFormInputName" srOnly>
+									Search
+								</Form.Label>
+								<div className="search">
+									<Form.Control
+										id="search-input"
+										name="searchTerm"
+										autoComplete="off"
+										placeholder="Search for tweets"
+									/>
+									<span>
+										<i className="fas fa-search" />
+									</span>
+								</div>
+							</Col>
+							<Col xs="auto" className="my-1">
+								<Button type="submit">Search</Button>
+							</Col>
+						</Form.Row>
+					</Form>
+				</div>
+
 				<TweetFeed tweetData={this.state.tweetData} />
 			</div>
 		);
