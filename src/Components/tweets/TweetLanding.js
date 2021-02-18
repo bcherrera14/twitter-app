781553ca234e7c8d0d4986a4c9973b92b35bb2e7
bcherrera14 <bcherrera14@gmail.com>
2021-02-18 07:36:56 -0800

    fix landing image size

--- src/Components/tweets/TweetLanding.js
+++ src/Components/tweets/TweetLanding.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Image } from 'react-bootstrap';
 
 const TweetLanding = () => {
-	return <div className="d-flex flex-column justify-content-center align-items-center headline landing-page" />;
+	return <div className="landing-page" />;
 };
 
 export default TweetLanding;
